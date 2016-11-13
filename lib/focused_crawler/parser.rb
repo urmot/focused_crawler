b0fb74747dab2a147950f622ac76b7ef707f93a0
muramoto <mura@DSE-MBP-13-9.local>
2016-11-13 21:51:01 +0900

    add Parser class

--- lib/focused_crawler/parser.rb
+++ lib/focused_crawler/parser.rb
@@ -1,4 +1,21 @@
+require 'nokogiri'
+
 module FocusedCrawler
   class Parser
+    def parse
+      pages.each do |page|
+      end
+    end
+
+    def pages
+      Dir.glob('pages/*').map do |path|
+        page = File.read path
+        Nokogiri::HTML page
+      end
+    end
+
+    def ready?
+      !Dir.glob('pages/*').empty?
+    end
   end
 end
