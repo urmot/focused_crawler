139ae1bca951165c21243751a4a5b9fb2d400454
muramoto <mura@DSE-MBP-13-9.local>
2016-11-14 00:34:44 +0900

    add Brain

--- lib/focused_crawler/brain.rb
+++ lib/focused_crawler/brain.rb
@@ -1,4 +1,49 @@
 module FocusedCrawler
   class Brain
+    def initialize
+      @crawler = Crawler.new
+      @parser = Parser.new
+    end
+
+    def start
+      prepare
+      crawl
+      parse
+    end
+
+    def prepare
+      return if prepared?
+
+      seed_urls = { url: 'http://yahoo.cp.jp', score: 0 }.to_json
+      File.write 'urls/seed.json', seed_urls
+    end
+
+    def prepared?
+      !Dir.glob('urls/*.json').empty?
+    end
+
+    def crawl
+      Thread.new do
+        loop do
+          sleep 3 unless @crawler.prepared?
+          @crawler.run
+          break if finish?
+        end
+      end
+    end
+
+    def parse
+      Thread.new do
+        loop do
+          sleep 3 unless @parser.prepared?
+          @parser.run
+          break if finish?
+        end
+      end
+    end
+
+    def finish?
+      @crawler.waiting? && @parser.waiting?
+    end
   end
 end
