96824ff51d95a1c3c5105d7a72d64b19aea75bcf
muramoto <mura@DSE-MBP-13-9.local>
2016-11-14 00:58:45 +0900

    add brain test

---
+++ test/brain_test.rb
@@ -0,0 +1,33 @@
+require 'test_helper'
+
+class BrainTest < Minitest::Test
+  def setup
+    @brain = FocusedCrawler::Brain.new
+  end
+
+  def teardown
+    Dir.glob('pages/*').each {|path| File.delete path }
+    Dir.glob('urls/*.json').each {|path| File.delete path }
+  end
+
+  def test_that_it_should_create_seed_page_urls_file_when_urls_dir_is_empty
+    Dir.glob('urls/*.json').each {|path| File.delelte path }
+    @brain.prepare
+    path = 'urls/seed.json'
+    assert { true == File.exist?(path) }
+  end
+
+  def test_that_it_should_do_nothing_when_crawler_is_prepared
+    in_test_urls do
+      @brain.prepare
+      assert { false == File.exist?('urls/seed.json') }
+    end
+  end
+
+  def test_that_it_should_be_finish
+    FocusedCrawler::Page.stub_any_instance(:page, 'test') do
+      @brain.start
+      assert true
+    end
+  end
+end
