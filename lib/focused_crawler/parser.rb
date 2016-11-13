4237df9b53ee864b9d891461045e31333cbbb723
muramoto <mura@DSE-MBP-13-9.local>
2016-11-13 23:48:36 +0900

    modified Parser#pages to not delete a page file

--- lib/focused_crawler/parser.rb
+++ lib/focused_crawler/parser.rb
@@ -27,7 +27,6 @@ module FocusedCrawler
     def pages
       Dir.glob('pages/*').map do |path|
         page = File.read path
-        File.delete path
         Nokogiri::HTML page
       end
     end
