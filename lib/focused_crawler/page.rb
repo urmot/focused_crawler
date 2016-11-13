6ee5f3db41b4f9f3baf9001a3bcb7e0429eb8e56
muramoto <mura@DSE-MBP-13-9.local>
2016-11-13 10:35:26 +0900

    add page

---
+++ lib/focused_crawler/page.rb
@@ -0,0 +1,33 @@
+require 'open-uri'
+require 'digest/sha1'
+
+module FocusedCrawler
+  class Page
+    attr_reader :url
+
+    def initialize(url)
+      @url = url
+    end
+
+    def save
+      File.write path, page
+    end
+
+    def page
+      @page ||= open(url) {|f| f.read.encode Encoding::UTF_8, f.charset }
+    end
+
+    def crawled?
+      File.exist? path
+    end
+
+    def path
+      filename = Digest::SHA1.hexdigest url
+      File.join directory, filename
+    end
+
+    def directory
+      File.absolute_path 'pages'
+    end
+  end
+end
