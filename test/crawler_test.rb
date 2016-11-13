624cb7f2c6cf88c061d7d30ef1d0729838fe024c
muramoto <mura@DSE-MBP-13-9.local>
2016-11-13 22:50:01 +0900

    remove Crawler#pages test

--- test/crawler_test.rb
+++ test/crawler_test.rb
@@ -12,24 +12,6 @@ class CrawlerTest < Minitest::Test
     end
   end
 
-  def test_that_it_should_get_urls_from_urls_directory
-    urls_example
-    assert { 3 == @crawler.pages.size }
-  end
-
-  def test_that_it_should_return_uniq_url_list
-    urls_example
-    urls_example
-    assert { 3 == @crawler.pages.size }
-  end
-
-  def test_that_it_should_return_sorted_url_list_by_score
-    urls_example
-    pages = @crawler.pages
-    expect = 3.times {|i| FocusedCrawler::Page.new("http://test#{i}.com") }
-    assert expect, pages
-  end
-
   def test_that_it_shoud_be_falsly_when_crawler_is_not_ready
     Dir.glob('urls/*').each {|path| File.delete path }
     assert { false == @crawler.ready? }
@@ -49,13 +31,5 @@ class CrawlerTest < Minitest::Test
         assert { 3 == pages.size }
       end
     end
-  end
-
-  def urls_example
-    content = Array.new(3) do |i|
-      { url: "http://test#{i}.com", score: i }
-    end.to_json
-    tempfile = Tempfile.new(['', '.json'], 'urls')
-    File.write tempfile.path, content
   end
 end
