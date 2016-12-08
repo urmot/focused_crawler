a00a044851bec762eee690ce4ca3e4ed38f73f75
muramoto <mura@DSE-MBP-13-9.local>
2016-12-08 22:48:14 +0900

    remove RelevanceCaluculator

--- lib/focused_crawler/classifier.rb
+++ lib/focused_crawler/classifier.rb
@@ -3,7 +3,6 @@ module FocusedCrawler
     attr_accessor :threshold
 
     def initialize(threshold = 0.5)
-      @calc = RelevanceCalculator.new
       @threshold = threshold
     end
 
@@ -19,7 +18,6 @@ module FocusedCrawler
     end
 
     def similarity
-      @calc.calculate @document.body
     end
   end
 end
